77ac5dd830598c0f2d9b4b9b4f0db84a7978dad8
mah-c <benuproj1@gmail.com>
2025-07-30 18:28:06 +0300

    Added all basic mathematical operators using 'if...else statement'

--- calculator.py
+++ calculator.py
@@ -1,3 +1,24 @@
-num1 = float(input('Enter a numer: '))
-num2 = float(input('Enter another number: '))
+num1 = float(input('Enter a number: '))
 operand = input('Enter a mathematical operator: ')
+num2 = float(input('Enter another number: '))
+
+if operand == '+':
+    result = num1 + num2
+    print(num1, operand, num2, ' = ', result)
+elif operand == '-':
+    result = num1 - num2
+    print(num1, operand, num2, ' = ', result)
+elif operand == '*':
+    result = num1 * num2
+    print(num1, operand, num2, ' = ', result)
+elif operand == '/':
+    result = num1 / num2
+    print(num1, operand, num2, ' = ', result)
+elif operand == '%':
+    result = num1 % num2
+    print(num1, operand, num2, ' = ', result)
+elif operand == '**':
+    result = num1 ** num2
+    print(num1, operand, num2, ' = ', result)
+else:
+    print('Invalid Operation')
